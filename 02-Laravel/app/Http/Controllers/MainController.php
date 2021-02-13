3d9267b76d8eb756cad0cdd266d54d44262f6451
Alejandro Mongua <alejandro.mongua@gmail.com>
2021-02-12 20:37:49 -0500

    Parámetros en la URL

--- 02-Laravel/app/Http/Controllers/MainController.php
+++ 02-Laravel/app/Http/Controllers/MainController.php
@@ -9,4 +9,8 @@ class MainController extends Controller
     public function welcome() {
         return view('Main.welcome');
     }
+
+    public function echo($mensaje) {
+        echo $mensaje;
+    }
 }
--- 02-Laravel/routes/web.php
+++ 02-Laravel/routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\MainController@welcome');
+Route::get('/echo/{mensaje}', 'App\Http\Controllers\MainController@echo');
