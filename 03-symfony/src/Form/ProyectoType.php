73ec6eede2c9c4334ccb9d722daf7253b3ea4b90
Alejandro Mongua <alejandro.mongua@gmail.com>
2021-02-28 18:50:17 -0500

    Agregado CRUD de Proyectos

---
+++ 03-symfony/src/Form/ProyectoType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Proyecto;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProyectoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('inicio')
+            ->add('fin')
+            ->add('estado')
+            ->add('descripcion')
+            ->add('generado_por_id')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Proyecto::class,
+        ]);
+    }
+}
