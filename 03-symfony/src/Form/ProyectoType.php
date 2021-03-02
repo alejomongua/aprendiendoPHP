e4f404a1785568debef2fa163532b7fb49a0f504
Alejandro Mongua <alejandro.mongua@gmail.com>
2021-03-01 22:33:19 -0500

    Se estiliza el formulario de creación del proyecto

--- 03-symfony/src/Form/ProyectoType.php
+++ 03-symfony/src/Form/ProyectoType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Proyecto;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,35 @@ class ProyectoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('inicio')
-            ->add('fin')
-            ->add('estado')
-            ->add('descripcion')
+            ->add('inicio', HiddenType::class)
+            ->add('fin', HiddenType::class)
+            ->add('estado', TextType::class, [
+                'label_attr' => [
+                    'class' => 'block m-4 leading-10'
+                ],
+                'attr' => [
+                    'class' => 'border-2 rounded shadow border-gray-700 p-2'
+                ],
+                'row_attr' => [
+                    'class' => 'mx-4 font-semibold'
+                ],
+            ])
+            ->add('descripcion', TextareaType::class, [
+                'label' => 'Descipción',
+                'label_attr' => [
+                    'class' => 'block m-4 leading-10'
+                ],
+                'attr' => [
+                    'class' => 'border-2 rounded shadow border-gray-700 p-2 w-full h-48'
+                ],
+                'row_attr' => [
+                    'class' => 'mx-4 font-semibold'
+                ],
+            ])
+            ->add('save', ButtonType::class, [
+                'attr' => ['class' => 'my-4 p-4 border rounded shadow bg-blue-300'],
+                'label' => 'Guardar',
+            ])
         ;
     }
 
